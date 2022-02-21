036a64320cb53b43d48db64a03f7f10d2659a736
Alexis Chau <79122383+alexischau@users.noreply.github.com>
2022-02-21 11:52:57 -0800

    Add files via upload

---
+++ ClassGraphQL.js
@@ -0,0 +1,57 @@
+import "./Class.css";
+import React, {useState, useEffect} from 'react';
+
+function ClassGraphQL(props) {
+    const [classInfo, setClassInfo] = useState({});
+
+    const url = "https://api.peterportal.org/rest/v0/graphql"
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const query = `
+                query {
+                    course(id:"${props.name}") {
+                        title
+                        despartment_name
+                        description
+                    }
+                }
+            
+            `
+
+            const response = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify({query}),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const data = await (await response).json();
+            console.log(data);
+            setClassInfo(data.data.course);
+        }
+        fetchData();
+    
+    }, [props.name]);
+
+    let info;
+    if (classInfo){
+        info = <div className="information">
+            <p id="title">{classInfo.title}</p>
+            <p id="department">{classInfo.department_name}</p>
+            <p id="description">{classInfo.description}</p>
+        </div>
+    } else if (classInfo == null){
+        info = <p>Class Not Found</p>
+    } else{
+        info = <p>Loading...</p>
+    }
+
+    return (
+        <div className="class">
+            {props.name}
+        </div>
+    )
+}
+
+export default ClassGraphQL;
